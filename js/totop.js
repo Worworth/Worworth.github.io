6dd971ff576c158f02c85cfd85c7c420dff3b57e
Worworth <b4ei.msarzal@gmail.com>
2022-04-30 14:33:40 -0300

    Casi definitiva
    Pagina de cero

--- js/totop.js
+++ js/totop.js
@@ -1,36 +1,80 @@
-//Get the button:
+//TO TOP//
+
+
 mybutton = document.getElementById("myBtn");
 mybutton2 = document.getElementById("myBtn2");
 mybutton3 = document.getElementById("myBtn3");
 
 
 /*// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};*/
 
-function scrollFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    mybutton.style.display = "block";
-    mybutton2.style.display= "block";
-    mybutton3.style.display= "block";
-  } else {
-    mybutton.style.display = "none";
-    mybutton2.style.display = "none";
-    mybutton3.style.display = "none";
-  }
-}
-
-*/
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 function topFunction2() {
-  var elenm =document.getElementById("productos2");
+  var elenm =document.getElementByTag("productos2");
   elenm.scrollIntoView()
 }
 function topFunction3() { 
   var elenm =document.getElementById("productos3");
   elenm.scrollIntoView()
+}
+
+//TABS//
+
+function openCity(evt, cityName) {
+
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+//MODAL BOX//
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img3.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
 }
