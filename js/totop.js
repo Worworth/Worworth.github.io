b25224bfa22f7e5755da07c9e6a328180eabd650
Worworth <b4ei.msarzal@gmail.com>
2022-05-03 01:32:41 -0300

    casi acabada

--- js/totop.js
+++ js/totop.js
@@ -14,8 +14,11 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 function topFunction2() {
-  var elenm =document.getElementByTag("productos2");
-  elenm.scrollIntoView()
+  //var elenm =document.getElementByTag("productos2");
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+  //elenm.scrollIntoView()
+
 }
 function topFunction3() { 
   var elenm =document.getElementById("productos3");
