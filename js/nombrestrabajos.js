1cb8b7c3c6a05d84e3d6b2d4b0d06dd4f3bd1838
Worworth <b4ei.msarzal@gmail.com>
2022-06-08 21:02:46 -0300

    Cambios en las pesrtañas de productros

---
+++ js/nombrestrabajos.js
@@ -0,0 +1,11 @@
+
+
+let button = document.getElementsByClassName('techintvm');
+let button2 = document.getElementsByClassName('boca');
+let button3 = document.getElementsByClassName('cesar');
+let button4 = document.getElementsByClassName('directorio');
+let button5 = document.getElementsByClassName('siderca');
+
+button.addEventlistener('mouseenter', e =>{
+    document.getElementsByClassName("vacam").style.display="block";
+});
